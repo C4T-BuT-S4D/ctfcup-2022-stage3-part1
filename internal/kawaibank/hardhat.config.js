7ccabc3af7657c98c72f2f6646dcd38b582b6150
b1r1b1r1 <48091103+b1r1b1r1@users.noreply.github.com>
2022-11-27 17:20:43 +0000

    Fixes

--- internal/kawaibank/hardhat.config.js
+++ internal/kawaibank/hardhat.config.js
@@ -7,11 +7,11 @@ module.exports = {
   solidity: "0.8.17",
   networks: {
     ctfpuc_private: {
-      url: `${process.env.BLOCKCHAIN_PROTOCOL}://${process.env.BLOCKCHAIN_TOKEN}@${config.BLOCKCHAIN_ADDRESS}:${config.BLOCKCHAIN_PORT}`,
+      url: `${config.BLOCKCHAIN_PROTOCOL}://${process.env.BLOCKCHAIN_TOKEN}@${config.BLOCKCHAIN_ADDRESS}:${config.BLOCKCHAIN_PORT}`,
       accounts: [config.DEPLOY_KEY]
     },
     ctfpuc_public: {
-      url: `${process.env.BLOCKCHAIN_PROTOCOL}://${config.BLOCKCHAIN_ADDRESS}:${config.BLOCKCHAIN_PORT}`,
+      url: `${config.BLOCKCHAIN_PROTOCOL}://${config.BLOCKCHAIN_ADDRESS}:${config.BLOCKCHAIN_PORT}`,
       accounts: [config.DEPLOY_KEY]
     }
   }
