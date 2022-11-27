b84144c956b398585b428fdd8e7565927b4d8e3a
Roman Nikitin <nikrom2012@me.com>
2022-11-27 17:50:33 +0000

    fix drone problems

--- internal/drone/validator/app.py
+++ internal/drone/validator/app.py
@@ -9,10 +9,14 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def index():
-    if str(request.json.get('repo', {}).get('id', -1)) not in ALLOWED_REPOS:
+    repo = request.json.get('repo', {}).get('slug', '<empty>')
+    config_data = request.json.get('config', {}).get('data', '')
+    config_hash = md5(config_data.encode()).hexdigest()
+    app.logger.info('new request from repo %s, config hash %s, correct %s', repo, config_hash, CORRECT_HASH)
+    if repo not in ALLOWED_REPOS:
         return jsonify({'message': 'repo not allowed'}), 400
-    if md5(request.json.get('config', {}).get('data', '').encode()).hexdigest() != CORRECT_HASH:
-        return jsonify({'message': 'you are forbidden to change the CI file'})
+    if config_hash != CORRECT_HASH:
+        return jsonify({'message': 'you are forbidden to change the CI file'}), 400
     return '', 200
 
 app.run(host='0.0.0.0', port=5000, debug=True)
