9522aa591abec54a47e3cc6f76ca06122542b9b3
b1r1b1r1 <48091103+b1r1b1r1@users.noreply.github.com>
2022-12-05 16:32:47 +0000

    Sort in hash checker

--- services/kawaibank/get_repo_fingerprint.sh
+++ services/kawaibank/get_repo_fingerprint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-find . -type f -not '(' -name '*.sol' -o -name '.drone.yml' ')' | xargs -P1 -n1 sha256sum
-find . -type f
+find . -type f -not '(' -name '*.sol' -o -name '.drone.yml' ')' -print0 | sort -z | xargs -r0 -P1 -n1 sha256sum
+find . -type f -print0 | sort -z
